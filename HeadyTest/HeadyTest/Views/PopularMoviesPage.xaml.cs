08610a6fe4c93c30e5fb83bd5a253fd61c6bf544
Saqeb Deshmukh <saqeb.deshmukh@gmail.com>
2020-02-23 19:45:09 +0530

    Add project files.

---
+++ HeadyTest/HeadyTest/Views/PopularMoviesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HeadyTest.Views
+{
+    public partial class PopularMoviesPage : ContentPage
+    {
+        public PopularMoviesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
