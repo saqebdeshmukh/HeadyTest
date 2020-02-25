90b99ad9f9ee0f1ec42ddfe7fdfc9ad1a72ecaae
Saqeb Deshmukh <saqeb.deshmukh@gmail.com>
2020-02-26 01:52:57 +0530

    Icons

--- HeadyTest/HeadyTest.Android/MainActivity.cs
+++ HeadyTest/HeadyTest.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Prism.Ioc;
 
 namespace HeadyTest.Droid
 {
-    [Activity(Label = "HeadyTest", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Heady Test", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
